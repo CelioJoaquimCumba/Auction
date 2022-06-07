04f675804d75819c1da01dc6d4f8dedbec271b52
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2022-06-07 23:50:16 +0200

    Added the ability to register an account(vulnearability: it can overwrite accounts). Added ability to login if the username matches the password. Changed the email field to username field

--- app/src/main/java/mz/ac/isutc/i33/auction/LoginActivity.java
+++ app/src/main/java/mz/ac/isutc/i33/auction/LoginActivity.java
@@ -1,5 +1,6 @@
 package mz.ac.isutc.i33.auction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,8 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
-    EditText email=null, password=null;
+    EditText username=null, password=null;
     Button login_button;
 
 
@@ -20,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email = findViewById(R.id.email_login);
+        username = findViewById(R.id.username_login);
         password = findViewById(R.id.password_login);
 
         login_button = findViewById(R.id.login_button_login);
@@ -28,15 +36,47 @@ public class LoginActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (validFields(
-                        email.getText().toString(),
+                if ( validFields(
+                        username.getText().toString(),
                         password.getText().toString())
+                        && userExists(
+                                username.getText().toString().trim(),
+                                password.getText().toString().trim() )
                 ){
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
             }
         });
+    }
+    private boolean userExists(String username, String password){
+        DatabaseReference reference = FirebaseDatabase.getInstance("https://auction-a4883-default-rtdb.firebaseio.com/").getReference("users");
+        Query user = reference.orderByChild("username").equalTo(username);
+        user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if( dataSnapshot.exists() ){
+                    String passwordFromDB = dataSnapshot.child(username).child("password").getValue(String.class);
+
+                    if(passwordFromDB.equals(password)){
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        startActivity(intent);
+                    }else{
+                        //TODO
+
+                    }
+                }else{
+                    //TODO
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return false;
     }
 
     public void toRegisterPage(View view) {
@@ -46,9 +86,9 @@ public class LoginActivity extends AppCompatActivity {
 
     //Validade is all the fields are field and the input of every each is valid
     //probably will need more enhancement
-    private boolean validFields(String email, String password){
+    private boolean validFields(String username, String password){
         if(
-                email.trim() != "" || password.trim() != ""
+                username.trim() == "" || password.trim() == ""
         ) {
             Toast.makeText(getApplicationContext(),
                     "One of your fields is not well putted. Guess which :)",
--- app/src/main/java/mz/ac/isutc/i33/auction/RegisterActivity.java
+++ app/src/main/java/mz/ac/isutc/i33/auction/RegisterActivity.java
@@ -2,6 +2,8 @@ package mz.ac.isutc.i33.auction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +11,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import mz.ac.isutc.i33.auction.models.User;
+
 public class RegisterActivity extends AppCompatActivity {
-    EditText username, email, password;
+    EditText username, email, password, passwordConfirmation;
     Button register_button;
+
+//    FirebaseDatabase rootNode;
+//    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +30,29 @@ public class RegisterActivity extends AppCompatActivity {
         username = findViewById(R.id.username_register);
         email = findViewById(R.id.email_register);
         password = findViewById(R.id.password_register);
+        passwordConfirmation = findViewById(R.id.confirmpassword_register);
 
         register_button = findViewById(R.id.register_button_register);
+
 
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username_txt = username.getText().toString();
+                String email_txt = email.getText().toString();
+                String password_txt = password.getText().toString();
+                String passwordConfirmation_txt = passwordConfirmation.getText().toString();
                 if(validFields(
-                        username.getText().toString(),
-                        email.getText().toString(),
-                        password.getText().toString()
+                        username_txt,
+                        email_txt,
+                        password_txt,
+                        passwordConfirmation_txt
                 )){
+                    FirebaseDatabase database = FirebaseDatabase.getInstance("https://auction-a4883-default-rtdb.firebaseio.com/");
+                    DatabaseReference myRef = database.getReference("users");
+
+                    User user = new User(username_txt,email_txt,password_txt);
+                    myRef.child(username_txt).setValue(user);
                     Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
@@ -39,8 +61,24 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     }
-    private boolean validFields(String username, String email, String password){
-        if( username.trim() != "" || email.trim() != "" || password.trim() != "" ) {
+    private  boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+    private boolean validFields(String username, String email, String password, String passwordConfirmation){
+        if( username.trim().equals("") ){
+            return false;
+        }
+        if( email.trim().equals("") ){
+            return false;
+        }
+        if( password.trim().equals("") ){
+            return false;
+        }
+        if( !password.equals(passwordConfirmation) ){
+            return false;
+        }
+
+        if( !isValidEmail(email) ) {
             Toast.makeText(getApplicationContext(),
                     "One of your fields is not well putted. Guess which :)",
                     Toast.LENGTH_LONG).show();
